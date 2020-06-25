6f1cf52a93c5d6bcd6983140fd3e063a8246d14d
unknown <clay2000lbs@gmail.com>
2020-06-25 18:10:35 -0500

    n/a

--- Article/Article.js
+++ Article/Article.js
@@ -111,3 +111,19 @@ const data = [
 
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 */
+
+//step 1: 
+function articleMakere() {
+  let article = document.createElement('div');
+  let articleTitle = document.createElement('h2');
+  let articleDate = document.createElement('p');
+  let articleContent = document.createElement('p');
+  let articleExpand = document.createElement('button');
+  
+  articleTitle.classList.add('article');
+
+  return article
+
+};
+
+
